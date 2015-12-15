763f085dd4ef516c89d601e27855c29fddfd0e85
schlagelk <kenny.schlagel+github@gmail.com>
2015-12-15 08:35:57 -0700

    add table view for beacons

--- blueshft/BeaconsViewController.swift
+++ blueshft/BeaconsViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class BeaconsViewController: UIViewController {
+class BeaconsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "groupcell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +36,15 @@ class BeaconsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
 }
